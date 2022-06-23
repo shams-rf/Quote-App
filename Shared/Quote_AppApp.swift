72e1b1ef0725c2b8491452d8cc372e081089491b
Shamsuddin Refaei <shamss.aldin@gmail.com>
2022-06-23 11:42:33 +0100

    Initial Commit

---
+++ Shared/Quote_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quote_AppApp.swift
+//  Shared
+//
+//  Created by Shamsuddin Refaei on 23/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Quote_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
