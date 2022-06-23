03fedd0b9a805f3edd491a0da2e90ceed587878c
Shamsuddin Refaei <shamss.aldin@gmail.com>
2022-06-23 16:44:08 +0100

    Add code to parse JSON file

--- Shared/Services/DataService.swift
+++ Shared/Services/DataService.swift
@@ -7,3 +7,54 @@
 
 import Foundation
 
+class DataService {
+    
+    static func getLocalData() -> [Quote] {
+        
+        //parse local json file
+        
+        //get url path to json file
+        let pathString = Bundle.main.path(forResource: "quotes", ofType: "json")
+        
+        //check if pathString is nil, otherwise...
+        guard pathString != nil else {
+            
+            return [Quote]()
+        }
+        
+        //create url object
+        let url = URL(fileURLWithPath: pathString!)
+        
+        do {
+            
+            //create data object
+            let data = try Data(contentsOf: url)
+            
+            //decode data with json decoder
+            let decoder = JSONDecoder()
+            
+            do {
+                
+                let quoteData = try decoder.decode([Quote].self, from: data)
+                
+                //add unique IDs
+                for q in quoteData {
+                    
+                    q.id = UUID()
+                }
+                
+                //return quotes
+                return quoteData
+            }
+            catch{
+                
+                print(error)
+            }
+        }
+        catch{
+            
+            print(error)
+        }
+        return [Quote]()
+    }
+}
